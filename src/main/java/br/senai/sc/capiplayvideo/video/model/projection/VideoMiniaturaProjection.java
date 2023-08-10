d40d529544a98d93cf50a101b788d299cf9a1deb
Pedro Henrique Berri <pedroberri@weg.net>
2023-08-10 19:25:21 -0300

    Pequenas atualizacoes

--- src/main/java/br/senai/sc/capiplayvideo/video/model/projection/VideoMiniaturaProjection.java
+++ src/main/java/br/senai/sc/capiplayvideo/video/model/projection/VideoMiniaturaProjection.java
@@ -18,6 +18,10 @@ public interface VideoMiniaturaProjection {
 
     LocalDate getPublicacao();
 
+    Long getVisualizacoes();
+
+    Long getCurtidas();
+
     @JsonIgnore
     String getCaminho();
 
